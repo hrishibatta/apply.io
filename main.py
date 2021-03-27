9c0f1379d840fb8d4c29b64aa78c0179cbdf8508
djsnyde3 <djsnyde3@ncsu.edu>
2021-03-27 18:21:56 -0400

    Rough implementation to apply for jobs

--- main.py
+++ main.py
@@ -63,96 +63,41 @@ class EasyApplyLinkedin:
         easy_apply_button = self.driver.find_element_by_xpath("//button[@aria-label='Easy Apply filter.' and @class='artdeco-pill artdeco-pill--slate artdeco-pill--2 artdeco-pill--choice ember-view search-reusables__filter-pill-button']")
         easy_apply_button.click()
 
+    def applyToJobs(self):
+        """This function applies to the jobs"""
+        QuickApply = self.driver.find_element_by_xpath("//button[@class='jobs-apply-button artdeco-button artdeco-button--3 artdeco-button--primary ember-view']")
+        QuickApply.click()
 
-    def find_offers(self):
-        """This function finds all the offers through all the pages result of the search and filter"""
+        time.sleep(5)
+        self.nextButton()
+        time.sleep(5)
+        self.reviewButton()
+        time.sleep(5)
+        self.submitApplication()
 
-        # find the total amount of results (if the results are above 24-more than one page-, we will scroll trhough all available pages)
-        total_results = self.driver.find_element_by_class_name("display-flex.t-12.t-black--light.t-normal")
-        total_results_int = int(total_results.text.split(' ',1)[0].replace(",",""))
-        print(total_results_int)
-
-        time.sleep(2)
-        # get results for the first page
-        current_page = self.driver.current_url
-        results = self.driver.find_elements_by_class_name("occludable-update.artdeco-list__item--offset-4.artdeco-list__item.p0.ember-view")
-
-        # for each job add, submits application if no questions asked
-        for result in results:
-            hover = ActionChains(self.driver).move_to_element(result)
-            hover.perform()
-            titles = result.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
-            for title in titles:
-                self.submit_apply(title)
-
-        # if there is more than one page, find the pages and apply to the results of each page
-        if total_results_int > 24:
-            time.sleep(2)
-
-            # find the last page and construct url of each page based on the total amount of pages
-            find_pages = self.driver.find_elements_by_class_name("artdeco-pagination__indicator.artdeco-pagination__indicator--number")
-            total_pages = find_pages[len(find_pages)-1].text
-            total_pages_int = int(re.sub(r"[^\d.]", "", total_pages))
-            get_last_page = self.driver.find_element_by_xpath("//button[@aria-label='Page "+str(total_pages_int)+"']")
-            get_last_page.send_keys(Keys.RETURN)
-            time.sleep(2)
-            last_page = self.driver.current_url
-            total_jobs = int(last_page.split('start=',1)[1])
-
-            # go through all available pages and job offers and apply
-            for page_number in range(25,total_jobs+25,25):
-                self.driver.get(current_page+'&start='+str(page_number))
-                time.sleep(2)
-                results_ext = self.driver.find_elements_by_class_name("occludable-update.artdeco-list__item--offset-4.artdeco-list__item.p0.ember-view")
-                for result_ext in results_ext:
-                    hover_ext = ActionChains(self.driver).move_to_element(result_ext)
-                    hover_ext.perform()
-                    titles_ext = result_ext.find_elements_by_class_name('job-card-search__title.artdeco-entity-lockup__title.ember-view')
-                    for title_ext in titles_ext:
-                        self.submit_apply(title_ext)
-        else:
-            self.close_session()
-
-    def submit_apply(self,job_add):
-        """This function submits the application for the job add found"""
-
-        print('You are applying to the position of: ', job_add.text)
-        job_add.click()
-        time.sleep(2)
-
-        # click on the easy apply button, skip if already applied to the position
+    def nextButton(self):
         try:
-            in_apply = self.driver.find_element_by_xpath("//button[@data-control-name='jobdetails_topcard_inapply']")
-            in_apply.click()
-        except NoSuchElementException:
-            print('You already applied to this job, go to next...')
+            nextButton = self.driver.find_element_by_xpath("//button[@aria-label='Continue to next step' and @class='artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
+            nextButton.click()
+        except:
             pass
-        time.sleep(1)
 
-        # try to submit if submit application is available...
+    def reviewButton(self):
         try:
-            submit = self.driver.find_element_by_xpath("//button[@data-control-name='submit_unify']")
-            submit.send_keys(Keys.RETURN)
+            review = self.driver.find_element_by_xpath("//button[@aria-label='Review your application' and @class='artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
+            review.click()
+        except:
+            pass
 
-        # ... if not available, discard application and go to next
-        except NoSuchElementException:
-            print('Not direct application, going to next...')
-            try:
-                discard = self.driver.find_element_by_xpath("//button[@data-test-modal-close-btn]")
-                discard.send_keys(Keys.RETURN)
-                time.sleep(1)
-                discard_confirm = self.driver.find_element_by_xpath("//button[@data-test-dialog-primary-btn]")
-                discard_confirm.send_keys(Keys.RETURN)
-                time.sleep(1)
-            except NoSuchElementException:
-                pass
-
-        time.sleep(1)
+    def submitApplication(self):
+        try:
+            submitApplication = self.driver.find_element_by_xpath("//button[@aria-label='Submit application' and @class='artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
+            submitApplication.click()
+        except:
+            pass
 
     def close_session(self):
         """This function closes the actual session"""
-
-        print('End of the session, see you later!')
         self.driver.close()
 
     def apply(self):
@@ -160,16 +105,15 @@ class EasyApplyLinkedin:
 
         self.driver.maximize_window()
         self.login_linkedin()
-        time.sleep(5)
-
+        time.sleep(2)
         self.url_generator()
-
-        time.sleep(5)
+        time.sleep(2)
         self.filter()
         time.sleep(2)
-        self.find_offers()
+
+        self.applyToJobs()
         time.sleep(2)
-        self.close_session()
+        #self.close_session()
 
 
 if __name__ == '__main__':
